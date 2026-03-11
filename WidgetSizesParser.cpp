a6c4c1f17fbb30b45f9c62466931a94f0b80cf1a
MyslivchenkoRI <MyslivchenkoRI@aeropribor.ru>
2026-03-11 15:33:15 +0300

    Update

--- WidgetSizesParser.cpp
+++ WidgetSizesParser.cpp
@@ -6,9 +6,11 @@
 #include <QSettings>
 #include <QDir>
 #include <QTimer>
+#include <QFileDialog>
 #include <QGridLayout>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
+#include <QProgressDialog>
 #include <QLabel>
 
 #include "MyCppDifferent.h"
@@ -17,6 +19,8 @@
 #include "MyQFileDir.h"
 #include "MyQDialogs.h"
 #include "MyQExecute.h"
+
+#include "Code.h"
 
 WidgetSizesParser::WidgetSizesParser(QWidget *parent)
 	: QWidget(parent)
@@ -33,53 +37,46 @@ WidgetSizesParser::WidgetSizesParser(QWidget *parent)
 
 	hlo1->addWidget(new QLabel("Path to scan:"));
 
-	//lePathToScan = new QLineEdit("D:\\Documents\\C++ QT\\Notes");
 	lePathToScan = new QLineEdit("C:\\");
 	hlo1->addWidget(lePathToScan);
 
 	QPushButton *btnScan = new QPushButton("Scan");
 	hlo1->addWidget(btnScan);
-	connect(btnScan,&QPushButton::clicked, this, &WidgetSizesParser::SlotScan);
+	connect(btnScan, &QPushButton::clicked, this, &WidgetSizesParser::SlotScan);
 
-	QPushButton *btnPause = new QPushButton("Pause");
-	hlo1->addWidget(btnPause);
-	connect(btnPause,&QPushButton::clicked, [](){ Item::pause = !Item::pause; });
+	Item::btnPause  = new QPushButton("Pause");
+	hlo1->addWidget(Item::btnPause);
+	connect(Item::btnPause, &QPushButton::clicked, [](){
+		Item::pause = !Item::pause;
+	});
 
-	QPushButton *btnAbort = new QPushButton("Abort");
+	QPushButton *btnAbort = new QPushButton("Stop");
 	hlo1->addWidget(btnAbort);
 	connect(btnAbort,&QPushButton::clicked, [](){ Item::Abort(); });
 
 	QPushButton *btnShowWarnings = new QPushButton(" Show warnings ");
 	hlo1->addWidget(btnShowWarnings);
-	connect(btnShowWarnings,&QPushButton::clicked, [](){
+	connect(btnShowWarnings, &QPushButton::clicked, [](){
 		MyQDialogs::ShowText("Warnings: has sizes 0 or -1: "+
 							 Item::seroSize.join("\n") + "\n" + Item::minusSize.join("\n"));
-
-
-//		QDir currentDir("C:\\");
-//		QFileInfoList entriesFI = currentDir.entryInfoList(
-//					QDir::NoDotAndDotDot | QDir::AllEntries | QDir::Hidden | QDir::System,
-//					QDir::DirsFirst // сначала папки
-//					);
-
-//		int entriesCount = entriesFI.size();
-
-//		QString content;
-//		for (int i=0; i<entriesCount; i++)
-//			if(entriesFI[i].filePath().startsWith("C:/usersuser1app"))
-//				content+= entriesFI[i].filePath()+ " "
-//					+ QSn(entriesFI[i].exists()) + " "
-//					+ QSn(entriesFI[i].isFile()) + " "
-//					+ QSn(entriesFI[i].isDir()) + " "
-//					+ QSn(QFile(entriesFI[i].filePath()).open(QFile::ReadOnly)) + " "
-//					+ QSn(QFile::copy(entriesFI[i].filePath(), "D:\\for_trash\\"+entriesFI[i].fileName())) + " "
-//					+ QSn(QFile::remove(entriesFI[i].filePath())) + " "
-//					+ "\n";
-//		MyQDialogs::ShowText(content);
-
 	});
 
 	hlo1->addStretch();
+
+	QPushButton *btnOpen = new QPushButton();
+	hlo1->addWidget(btnOpen);
+	btnOpen->setIcon(QApplication::style()->standardIcon(QStyle::StandardPixmap::SP_DialogOpenButton));
+	connect(btnOpen,&QPushButton::clicked, [this](){ LoadItems(); });
+
+	QPushButton *btnCheck = new QPushButton();
+	hlo1->addWidget(btnCheck);
+	btnCheck->setIcon(QApplication::style()->standardIcon(QStyle::StandardPixmap::SP_BrowserReload));
+	connect(btnCheck,&QPushButton::clicked, [this](){ CheckItems(); });
+
+	QPushButton *btnSave = new QPushButton();
+	hlo1->addWidget(btnSave);
+	btnSave->setIcon(QApplication::style()->standardIcon(QStyle::StandardPixmap::SP_DialogSaveButton));
+	connect(btnSave,&QPushButton::clicked, [this](){ SaveItems(scannedItems); });
 
 	Item::progress2 = new QLabel;
 	Item::progress3 = new QLabel;
@@ -113,8 +110,8 @@ WidgetSizesParser::WidgetSizesParser(QWidget *parent)
 			auto *item = static_cast<Item*>(rawPtr);
 			if (!item) return;
 
-			for (Item &child : item->subitems) {
-				CreateTreeItem(treeItem, &child);
+			for (auto &child : item->subitems) {
+				CreateTreeItem(treeItem, child.get());
 			}
 		}
 	});
@@ -165,17 +162,147 @@ void WidgetSizesParser::SlotScan()
 	CreateTreeItem(nullptr, &scannedItems);
 }
 
+void WidgetSizesParser::SaveItems(const Item &items)
+{
+	auto file = QFileDialog::getSaveFileName(this, "Select file", "", "*.txt");
+	if(file.isEmpty()) return;
+
+	QFile qfile(file);
+	if(!qfile.open(QFile::WriteOnly))
+	{
+		QMbError("Can't open file "+file);
+		return;
+	}
+
+	QTextStream stream(&qfile);
+	stream.setCodec(QTextCodec::codecForName("UTF-8"));
+
+	QProgressDialog dialog(this);
+	dialog.setMinimumDuration(1000);
+	dialog.setWindowTitle("Подготовка");
+	dialog.setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::CustomizeWindowHint);
+	dialog.setCancelButton(nullptr);
+	dialog.setRange(0, 0);
+
+	QApplication::processEvents();
+	auto all = items.ItemsList();
+
+	dialog.setRange(0, all.size());
+	dialog.setWindowTitle("Запись");
+	dialog.setValue(0);
+	QApplication::processEvents();
+	for(auto &item:all)
+	{
+		item->SaveItem(stream, false);
+		dialog.setValue(dialog.value()+1);
+		QApplication::processEvents();
+	}
+}
+
+void WidgetSizesParser::LoadItems()
+{
+	auto fileName = QFileDialog::getOpenFileName(this, "Select file", "", "*.txt");
+	if (fileName.isEmpty()) return;
+
+	QFile qfile(fileName);
+	if (!qfile.open(QFile::ReadOnly)) {
+		QMbError("Can't open file " + fileName);
+		return;
+	}
+
+	qint64 totalSize = qfile.size();
+	int readedLinesCnt = 0;
+
+	QTextStream stream(&qfile);
+	stream.setCodec(QTextCodec::codecForName("UTF-8"));
+
+	QVector<Item*> stack;
+	scannedItems = Item();
+
+	QProgressDialog dialog(this);
+	dialog.setMinimumDuration(1000);
+	dialog.setWindowTitle("Чтение");
+	dialog.setWindowFlags(Qt::Dialog | Qt::WindowTitleHint | Qt::CustomizeWindowHint);
+	dialog.setCancelButton(nullptr);
+	dialog.setRange(0, 100);
+
+	while (!stream.atEnd()) {
+		QString line = stream.readLine();
+		readedLinesCnt++;
+
+		if(readedLinesCnt % 300 == 0)
+		{
+			int percent = static_cast<int>((qfile.pos() * 100) / totalSize);
+			dialog.setValue(percent);
+			QApplication::processEvents();
+		}
+
+		if (line.trimmed().isEmpty()) continue;
+
+		Item newItem;
+		if (QString error = newItem.LoadItem(line); !error.isEmpty()) {
+			QMbError("Line parse error: " + error);
+			return;
+		}
+
+		// Если это первый элемент файла (корень)
+		if (stack.isEmpty()) {
+			if(newItem.depth != 1)
+			{
+				QMbError("Error depth in line: " + line);
+				return;
+			}
+			scannedItems = std::move(newItem);
+			stack.push_back(&scannedItems);
+			continue;
+		}
+
+		// Ищем родителя: поднимаемся по стеку, пока глубина последнего в стеке не станет меньше текущего
+		while (!stack.isEmpty() && stack.back()->depth >= newItem.depth) {
+			stack.pop_back();
+		}
+
+		if (stack.isEmpty()) {
+			QMbError("Ошибка структуры: обнаружен лишний корневой элемент или нарушена вложенность");
+			return;
+		}
+
+		// 4. Добавление элемента
+		stack.back()->subitems.emplace_back(std::make_unique<Item>(std::move(newItem)));
+		// Кладем указатель на только что добавленный элемент в стек
+		stack.push_back(stack.back()->subitems.back().get());
+	}
+
+	tree->clear();
+	CreateTreeItem(nullptr, &scannedItems);
+	QMbInfo("Чтение завершено");
+}
+
+void WidgetSizesParser::CheckItems()
+{
+	auto alternalScannedItems = Item::DoCompleteScan(scannedItems.itemPathWithName);
+
+	if(alternalScannedItems == scannedItems)
+	{
+		QMbInfo("Сверка отличий не обнаружила");
+	}
+	else
+	{
+		auto answ = QMessageBox::question(this, "", "При сверке обнаружены отличия, сохранить их?");
+		if(answ == QMessageBox::Yes)
+		{
+			SaveItems(alternalScannedItems);
+		}
+	}
+}
+
 QTreeWidgetItem *WidgetSizesParser::CreateTreeItem(QTreeWidgetItem *parent, Item *item)
 {
 	QString sizeStr = item->size == -1
 			? ""
 			: QLocale().formattedDataSize(item->size);
 
-	QString name = item->m_info.fileName();
-	QStringList vals = MyQString::QStringListSized(2);
-	if(name.isEmpty()) vals[0] = item->m_info.filePath();
-	else vals[0] = std::move(name);
-	vals[1] = sizeStr;
+	QStringList vals = { item->itemNameNoPath, sizeStr };
 
 	QTreeWidgetItem *treeItem = new QTreeWidgetItem(vals);
 
@@ -183,7 +310,7 @@ QTreeWidgetItem *WidgetSizesParser::CreateTreeItem(QTreeWidgetItem *parent, Item
 	QVariant ptrVariant = QVariant::fromValue<void*>(item);
 	treeItem->setData(0, Qt::UserRole, ptrVariant);
 
-	treeItem->setData(1, Qt::UserRole, QVariant(item->m_info.filePath()));
+	treeItem->setData(1, Qt::UserRole, QVariant(item->itemPathWithName));
 
 	if (!item->subitems.empty()) {
 		// Добавим фиктивного ребенка, чтобы появился значок раскрытия
@@ -200,23 +327,26 @@ QTreeWidgetItem *WidgetSizesParser::CreateTreeItem(QTreeWidgetItem *parent, Item
 
 void Item::InitAndScan(QFileInfo &&info, int deps_)
 {
-	while(pause and !abort) { MyCppDifferent::sleep_ms(10); QCoreApplication::processEvents(); }
+	while(pause and !abort) { Item::btnPause->setText("Continue"); MyCppDifferent::sleep_ms(10); QCoreApplication::processEvents(); }
+	Item::btnPause->setText("Pause");
 	if(abort) { return; }
 
 	depth = deps_+1;
-	m_info = std::move(info);
-	size = m_info.isDir() ? -1 : m_info.size();
+	size = info.isDir() ? -1 : info.size();
 
-	if(m_info.isSymLink()) { return; }
+	if(info.isSymLink()) { return; }
 
-	if (!m_info.isDir()) // если это не каталог
+	itemPathWithName = info.filePath();
+	itemNameNoPath = info.fileName();
+
+	if (!info.isDir()) // если это не каталог
 	{
-		if(m_info.size() == 0) seroSize += "size = 0 " + m_info.filePath();
-		else if(m_info.size() == -1) minusSize += "size = -1 " + m_info.filePath();
+		if(info.size() == 0) seroSize += "size = 0 " + info.filePath();
+		else if(info.size() == -1) minusSize += "size = -1 " + info.filePath();
 		return;
 	}
 
-	QDir currentDir(m_info.filePath());
+	QDir currentDir(info.filePath());
 	QFileInfoList entriesFI = currentDir.entryInfoList(
 				QDir::NoDotAndDotDot | QDir::AllEntries | QDir::Hidden | QDir::System,
 				QDir::DirsFirst // сначала папки
@@ -232,25 +362,65 @@ void Item::InitAndScan(QFileInfo &&info, int deps_)
 
 		PrintProgress(i, entriesCount, entriesFI[i]);
 
-		while(pause and !abort) { MyCppDifferent::sleep_ms(10); QCoreApplication::processEvents(); }
+		while(pause and !abort) { Item::btnPause->setText("Continue"); MyCppDifferent::sleep_ms(10); QCoreApplication::processEvents(); }
+		Item::btnPause->setText("Pause");
 		if(abort) { return; }
 
-		subitems.emplace_back(std::move(entriesFI[i]), depth);
-		size += subitems.back().size;
+		subitems.emplace_back(std::make_unique<Item>(std::move(entriesFI[i]), depth));
+		size += subitems.back()->size;
 
 		PrintProgress(i+1, entriesCount, entriesFI[i]);
 	}
+}
+
+std::vector<Item*> Item::ItemsList() const
+{
+	std::vector<Item*> vect;
+	ItemsList(vect);
+	return vect;
+}
+
+void Item::ItemsList(std::vector<Item*> &vect) const
+{
+	vect.push_back(const_cast<Item*>(this));
+	for(auto &item:subitems) item->ItemsList(vect);
+}
+
+void Item::SaveItem(QTextStream &stream, bool deep) const
+{
+	stream << "\"" << itemPathWithName << "\" \"" << itemNameNoPath << "\" " << size << " " << depth << "\n";
+	if(deep) for(auto &item:subitems) item->SaveItem(stream, deep);
+}
+
+QString Item::LoadItem(const QString &line)
+{
+	auto words = Code::CommandToWords(line);
+	if(words.size() < 4) return "words size error ("+QSn(words.size())+")";
+	if(not TextConstant::IsItTextConstant(words[0], false)) return "word 0 ("+words[0]+") is not text constant";
+	if(not TextConstant::IsItTextConstant(words[1], false)) return "word 1 ("+words[1]+") is not text constant";
+
+	itemPathWithName = TextConstant::GetTextConstVal(words[0], false);
+	itemNameNoPath = TextConstant::GetTextConstVal(words[1], false);
+
+	bool ok;
+	size = words[2].toLongLong(&ok);
+	if(not ok) return "word 2 ("+words[2]+") is not ulonglong";
+
+	depth = words[3].toUShort(&ok);
+	if(not ok) return "word 3 ("+words[3]+") is not ushort";
+
+	return "";
 }
 
 void Item::PrintProgress(int n, int total, const QFileInfo &childFI)
 {
 	if(depth == 1) {
 		progress2->setText("Обработано " + QSn(n) + " из " + QSn(total));
-		progress2_2->setText(m_info.filePath());
+		progress2_2->setText(itemPathWithName);
 	}
 	if(depth == 2){
 		progress3->setText(":  " + QSn(n) + " из " + QSn(total));
-		progress3_2->setText("  "+m_info.fileName() + "    /    " + childFI.fileName());
+		progress3_2->setText("    /    "+itemNameNoPath + "    /    " + childFI.fileName());
 	}
 
 	QCoreApplication::processEvents();
@@ -260,6 +430,7 @@ Item Item::DoCompleteScan(QString path)
 {
 	abort = false;
 	pause = false;
+	Item::btnPause->setText("Pause");
 	seroSize.clear();
 	minusSize.clear();
 
@@ -269,4 +440,29 @@ Item Item::DoCompleteScan(QString path)
 	else QMbInfo("Complete with warnings");
 
 	return result;
+}
+
+QString Compare(const Item &l, const Item &r)
+{
+	if(r.itemPathWithName != l.itemPathWithName) return r.itemPathWithName + " != " + l.itemPathWithName;
+	if(r.itemNameNoPath != l.itemNameNoPath) return r.itemNameNoPath + " != " + l.itemNameNoPath;
+	if(r.size != l.size) return QSn(r.size) + " != " + QSn(l.size);
+	if(r.depth != l.depth) return QSn(r.depth) + " != " + QSn(l.depth);
+	if(r.subitems.size() != l.subitems.size()) return QSn(r.subitems.size()) + " != " + QSn(l.subitems.size());
+	for(uint i=0; i<l.subitems.size(); i++)
+	{
+		auto res = Compare(*l.subitems[i], *r.subitems[i]);
+		if(not res.isEmpty()) return res;
+	}
+	return "";
+}
+
+bool operator==(const Item &l, const Item &r)
+{
+	return Compare(l, r).isEmpty();
+}
+
+bool operator!=(const Item &l, const Item &r)
+{
+	return not (l == r);
 }
--- WidgetSizesParser.h
+++ WidgetSizesParser.h
@@ -1,6 +1,9 @@
 #ifndef WIDGETSIZESPARSER_H
 #define WIDGETSIZESPARSER_H
 
+#include <memory>
+
+#include <QTextStream>
 #include <QWidget>
 #include <QPushButton>
 #include <QTreeWidget>
@@ -8,16 +11,27 @@
 #include <QFileInfo>
 #include <QLabel>
 
+struct Item;
+
+using ItemUptr = std::unique_ptr<Item>;
+
 struct Item
 {
-	QFileInfo m_info;
+	QString itemPathWithName;
+	QString itemNameNoPath;
 	qint64 size = -1;
 	int depth = 0;
-	std::vector<Item> subitems;
+	std::vector<ItemUptr> subitems;
+	Item *parent = nullptr;
 
 	Item() {}
 	Item(QFileInfo &&info, int deps_) { InitAndScan(std::move(info), deps_); }
 	void InitAndScan(QFileInfo &&info, int deps_);
+	std::vector<Item*> ItemsList() const;
+	void ItemsList(std::vector<Item*> &vect) const;
+
+	void SaveItem(QTextStream &stream, bool deep) const;
+	QString LoadItem(const QString &line);
 
 	void PrintProgress(int n, int total, const QFileInfo &childFI);
 
@@ -25,6 +39,7 @@ struct Item
 	inline static QLabel *progress3;
 	inline static QLabel *progress2_2;
 	inline static QLabel *progress3_2;
+	inline static QPushButton *btnPause;
 
 	inline static Item DoCompleteScan(QString path);
 	inline static void Abort() { abort=true; }
@@ -34,6 +49,10 @@ struct Item
 	inline static QStringList seroSize;
 	inline static QStringList minusSize;
 };
+
+inline QString Compare(const Item &l, const Item &r);
+inline bool operator== (const Item &l, const Item &r);
+inline bool operator!= (const Item &l, const Item &r);
 
 class WidgetSizesParser : public QWidget
 {
@@ -50,8 +69,12 @@ public:
 	void SlotScan();
 
 	Item scannedItems;
+	void SaveItems(const Item &items);
+	void LoadItems();
+	void CheckItems();
 
 	QTreeWidgetItem* CreateTreeItem(QTreeWidgetItem *parent, Item *item);
+
 };
 
 #endif // WIDGETSIZESPARSER_H
